5c6a15658dd61e5398e80bbf0af2119053a78051
Viktoria Korotkaya <viki.korotkaya@gmail.com>
2016-12-07 22:15:46 +0300

    correction 1

--- sedona/js/script.js
+++ sedona/js/script.js
@@ -1,6 +1,8 @@
 "use strict";
 function openForm() {
 	document.querySelector(".modal-content").classList.add("modal-content-show");
+	document.querySelector(".modal-overlay").style.display = 'block';
+
 };
 document.querySelector(".child .count-minus").addEventListener("click", function() {
 	var val = document.querySelector(".child-number").getAttribute('value');
@@ -31,7 +33,27 @@ document.querySelector(".adult .count-plus").addEventListener("click", function(
 	document.querySelector(".adult-number").setAttribute('value', val);
 	
 });
-
-
-
+/*window.addEventListener('click', function( e )
+  if (e.target == document.querySelector(".modal-content"))
+    document.querySelector(".modal-content").classList.remove("modal-content-show");
+  }
+}, false);*/
+document.querySelector(".modal-overlay").addEventListener("click", function(){
+			
+				if (document.querySelector(".modal-content").classList.contains("modal-content-show")) {
+					document.querySelector(".modal-content").classList.remove("modal-content-show");
+					document.querySelector(".modal-overlay").style.display = 'none';
+				}
+});
+/*document.querySelector(".search-form").parentNode.onclick = function () {
+    document.querySelector(".modal-content").classList.remove("modal-content-show");
+}*/
+window.addEventListener("keydown", function(){
+			if (event.keyCode === 27) {
+				if (document.querySelector(".modal-content").classList.contains("modal-content-show")) {
+					document.querySelector(".modal-content").classList.remove("modal-content-show");
+					document.querySelector(".modal-overlay").style.display = 'none';
+				}
+			}
+});
 
