d877165702dfab2422e56576743b3896bacaa542
Viktoria Korotkaya <viki.korotkaya@gmail.com>
2016-12-08 12:31:03 +0300

    corrections5

--- sedona/js/script.js
+++ sedona/js/script.js
@@ -4,7 +4,7 @@ function openForm() {
 	document.querySelector(".modal-overlay").style.display = 'block';
 
 };
-document.querySelector(".child .count-minus").addEventListener("click", function() {
+document.querySelector(".count-child .count-minus").addEventListener("click", function() {
 	var val = document.querySelector(".child-number").getAttribute('value');
 	if (val > 0) {
 		val = val - 1;
@@ -12,14 +12,14 @@ document.querySelector(".child .count-minus").addEventListener("click", function
 	}
 });
 
-document.querySelector(".child .count-plus").addEventListener("click", function() {
+document.querySelector(".count-child .count-plus").addEventListener("click", function() {
 	var val = document.querySelector(".child-number").getAttribute('value');
 	val = +val + 1;
 	document.querySelector(".child-number").setAttribute('value', val);
 	
 });
 
-document.querySelector(".adult .count-minus").addEventListener("click", function() {
+document.querySelector(".count-adult .count-minus").addEventListener("click", function() {
 	var val = document.querySelector(".adult-number").getAttribute('value');
 	if (val > 0) {
 		val = val - 1;
@@ -27,7 +27,7 @@ document.querySelector(".adult .count-minus").addEventListener("click", function
 	}
 });
 
-document.querySelector(".adult .count-plus").addEventListener("click", function() {
+document.querySelector(".count-adult .count-plus").addEventListener("click", function() {
 	var val = document.querySelector(".adult-number").getAttribute('value');
 	val = +val + 1;
 	document.querySelector(".adult-number").setAttribute('value', val);
